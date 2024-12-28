3726062349fa962e538e92ea9cf93aaad5829a5c
Yadzuka <yadzuka@eustrosoft.org>
2024-12-28 18:13:11 +0300

    api was changed to utils.js

--- etc/prod/webapps/ROOT/js/qrdemo/api.js
+++ etc/prod/webapps/ROOT/js/qrdemo/api.js
@@ -1,5 +1,4 @@
-import { openFile } from "./files";
-import { emptyOrUndefined } from "./utils";
+import { emptyOrUndefined } from "./utils.js";
 
 export const QR_DEMO_API_DEV = `${window.location.protocol}//${window.location.hostname}:9983/qr/v1/api/`
 export const QR_DEMO_API = `${window.location.protocol}//${window.location.hostname}/qrCodeDemo/v1/api/`
