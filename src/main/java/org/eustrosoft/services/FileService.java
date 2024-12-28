e1492a93fe9f6e6940bc8b4475231f3c2e5f5280
Yadzuka <yadzuka@eustrosoft.org>
2024-12-28 17:16:48 +0300

    File name in file download was added

--- src/main/java/org/eustrosoft/services/FileService.java
+++ src/main/java/org/eustrosoft/services/FileService.java
@@ -102,6 +102,7 @@ public class FileService {
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Type", file.getFileType());
         headers.add("Content-Length", file.getFileSize().toString());
+        headers.add("Content-Disposition", String.format("attachment; filename*=UTF-8''%s", file.getFileName()));
         return new ResponseEntity<>(
                 fileData, headers, HttpStatus.OK
         );
