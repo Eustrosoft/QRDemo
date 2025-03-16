a9e6bfbaab46b848b4a2897b7d12a9ce85cef858
Yadzuka <yadzuka@eustrosoft.org>
2025-03-16 14:53:04 +0300

    History file was updated in migration

--- dev/db/qrDemo/migration/migration_history.sql
+++ dev/db/qrDemo/migration/migration_history.sql
@@ -139,3 +139,80 @@ CREATE FUNCTION qrdemo.do_h_qr_range() RETURNS trigger
 $$;
 
 CREATE OR REPLACE TRIGGER qrdemo_qr_range_audit_trig AFTER INSERT OR DELETE OR UPDATE ON qrdemo.qr_range FOR EACH ROW EXECUTE FUNCTION qrdemo.do_h_qr_range();
+
+CREATE TABLE qrdemo.h_file (
+    file_name       character varying(256),
+    file_type       character varying(128),
+    extension       character varying(64),
+    active          boolean,
+    last_accessed   timestamp,
+    checksum        character varying(128),
+    public          boolean,
+    storage_place   character varying(64),
+    storage_path    character varying(256),
+    file_data       bytea,
+    file_size       bigint
+) INHERITS (qrdemo.h_entity);
+
+CREATE FUNCTION qrdemo.do_h_file() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+    BEGIN
+        --
+        -- Добавление строки в emp_audit, которая отражает операцию, выполняемую в emp;
+        -- для определения типа операции применяется специальная переменная TG_OP.
+        --
+        IF (TG_OP = 'DELETE') THEN
+            INSERT INTO qrdemo.h_file SELECT 'D', now(), OLD.*;
+            RETURN OLD;
+        ELSIF (TG_OP = 'UPDATE') THEN
+            IF (OLD = NEW) THEN
+             RETURN NEW;
+            END IF;
+            INSERT INTO qrdemo.h_file SELECT 'C', now(), OLD.*;
+            RETURN NEW;
+        END IF;
+        RETURN NULL; -- возвращаемое значение для триггера AFTER игнорируется
+    END;
+$$;
+
+CREATE OR REPLACE TRIGGER qrdemo_file_audit_trig AFTER INSERT OR DELETE OR UPDATE ON qrdemo.file FOR EACH ROW EXECUTE FUNCTION qrdemo.do_h_file();
+
+CREATE TABLE qrdemo.h_participant (
+    username      character varying(128),
+    password      character varying(256),
+    email         character varying(256),
+    referer       bigint,
+    lei           character varying(256),
+    address       character varying(256),
+    website       character varying(256),
+    organization  character varying(256),
+    active        BOOLEAN,
+    banned        BOOLEAN,
+    settings      character varying(2048),
+    banned_reason character varying(512)
+) INHERITS (qrdemo.h_entity);
+
+CREATE FUNCTION qrdemo.do_h_participant() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+    BEGIN
+        --
+        -- Добавление строки в emp_audit, которая отражает операцию, выполняемую в emp;
+        -- для определения типа операции применяется специальная переменная TG_OP.
+        --
+        IF (TG_OP = 'DELETE') THEN
+            INSERT INTO qrdemo.h_participant SELECT 'D', now(), OLD.*;
+            RETURN OLD;
+        ELSIF (TG_OP = 'UPDATE') THEN
+            IF (OLD = NEW) THEN
+             RETURN NEW;
+            END IF;
+            INSERT INTO qrdemo.h_participant SELECT 'C', now(), OLD.*;
+            RETURN NEW;
+        END IF;
+        RETURN NULL; -- возвращаемое значение для триггера AFTER игнорируется
+    END;
+$$;
+
+CREATE OR REPLACE TRIGGER qrdemo_participant_audit_trig AFTER INSERT OR DELETE OR UPDATE ON qrdemo.participant FOR EACH ROW EXECUTE FUNCTION qrdemo.do_h_participant();
