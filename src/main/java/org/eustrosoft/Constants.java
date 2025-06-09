21b262712c148a6ac17fa4c107b1b30e1479838f
Yadzuka <yadzuka@eustrosoft.org>
2025-06-09 22:37:52 +0300

    Versions of app was updated

--- src/main/java/org/eustrosoft/Constants.java
+++ src/main/java/org/eustrosoft/Constants.java
@@ -3,7 +3,7 @@ package org.eustrosoft;
 public class Constants {
 
     public static class Version {
-        public static final String APPLICATION_VERSION = "v0.2.5";
+        public static final String APPLICATION_VERSION = "v0.2.6";
 
         public static final String APPLICATION_VERSION_TEXT = "Версия приложения: " + APPLICATION_VERSION;
     }
